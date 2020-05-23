6337f0dc0b870e3cb954092dd39833df425b82e2
jimsey <the.james.burton@gmail.com>
2020-05-23 11:44:24 +0100

    added cors support

--- src/main/java/com/jimsey/projects/projecteuler/controllers/EulerController.java
+++ src/main/java/com/jimsey/projects/projecteuler/controllers/EulerController.java
@@ -3,6 +3,7 @@ package com.jimsey.projects.projecteuler.controllers;
 import com.jimsey.projects.projecteuler.services.EulerSolutionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ public class EulerController {
     @Autowired
     EulerSolutionService euler;
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping(value = "/smoke", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<Long> smoke() {
         return euler.smoke();
